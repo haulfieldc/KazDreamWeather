bb2d6663435eff3fdf7571fa050470fc6676e9a2
colden <haulfieldc@gmail.com>
2023-03-18 02:43:07 +0600

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "KazDreamWeather"
+include(":androidApp")
+include(":shared")
---
+++ shared/src/commonMain/kotlin/com/example/kazdreamweather/Platform.kt
@@ -0,0 +1,7 @@
+package com.example.kazdreamweather
+
+interface Platform {
+    val name: String
+}
+
+expect fun getPlatform(): Platform
